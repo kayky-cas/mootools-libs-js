c670f1301e6d8d970acd0fb5320a868bb75d4e1d
Kayky Casagrande <kayky@pop-os.localdomain>
2020-11-14 22:31:22 -0300

    classes

--- js/script.js
+++ js/script.js
@@ -1,42 +1,61 @@
+window.addEvent('domready', function() {
+
+    $$('#btn-deixar-vermelho')
+        .addEvent('click', function() {
+
+            $$('.deixar-vermelho').setStyle('color', 'red');
+
+        }
+    );
+
+
+
+    $$('#btn-event-exemplo')
+        .addEvent('click', function() {
+            alert('O botão foi clickado!');
+        });
+
+    $$('#btn-event-exemplo')
+        .addEvent('mouseover', function() {
+
+            if (this.getStyle('background-color') !== 'red') {
+
+                this.setStyle('background-color', 'red')
+            }
+            else {
+
+                this.setStyle('background-color', 'blue')
+            }
+
+        });
 
 
 
 
-    
 
+    var Animal = new Class({
 
-$$('#btn-deixar-vermelho')
-    .addEvent('click', function() {
-
-        $$('.deixar-vermelho').setStyle('color', 'red');
-
-    }
-);
-
-
-
-$$('#btn-event-exemplo')
-    .addEvent('click', function() {
-        alert('O botão foi clickado!');
+        initialize: function(tipo) {
+            this.tipo = tipo;
+        }
     });
 
-$$('#btn-event-exemplo')
-    .addEvent('mouseover', function() {
+    var Gato = new Class({
 
-        if (this.getStyle('background-color') !== 'red') {
-
-            this.setStyle('background-color', 'red')
+        Extends: Animal,
+        initialize: function(nome, cor) {
+            this.parent('gato');
+            this.nome = nome;
+            this.cor = cor;
         }
-        else {
+    })
 
-            this.setStyle('background-color', 'blue')
-        }
-
-    });
+    var meuGato = new Gato('Luke', 'branco');
 
 
 
-
+    $$('#p-gato')
+        .set('text', `Meu ${meuGato.tipo} se chama ${meuGato.nome} e ele é ${meuGato.cor}`);
 
 
 
@@ -45,4 +64,6 @@ $$('#btn-event-exemplo')
 
 
 
-    
+
+        
+});
